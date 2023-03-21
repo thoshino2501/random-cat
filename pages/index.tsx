71238909eee6a9b2be6e7a73760720c40ca7a14d
thoshino2501 <thoshino5270@gmail.com>
2023-03-22 01:12:23 +0900

    ボタンメッセージを変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
     return (
         <div className={styles.page}>
             <button onClick={handleClick} className={styles.button}>\
-                他のにゃんこも見る
+                きょうのにゃんこ🐱
             </button>
             <div className={styles.frame}>
                 {loading || <img src={imageUrl} className={styles.img} />}
